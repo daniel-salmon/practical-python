64e6d0905cec8255f533648912ae56ce43f1906c
daniel-salmon <me@daniel-salmon.com>
2023-12-15 23:33:44 -0500

    Add exercises for lesson 6.4

---
+++ Work/ticker.py
@@ -0,0 +1,44 @@
+# ticker.py
+
+from follow import follow
+from report import read_portfolio
+from tableformat import create_formatter
+import csv
+
+def filter_symbols(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
+
+def convert_types(rows, types):
+    for row in rows:
+        yield [t(r) for t, r in zip(types, row)]
+
+def make_dicts(rows, headers):
+    for row in rows:
+        yield dict(zip(headers, row))
+
+def select_columns(rows, indices):
+    for row in rows:
+        yield [row[index] for index in indices]
+
+def parse_stock_data(lines):
+    rows = csv.reader(lines)
+    rows = select_columns(rows, [0, 1, 4])
+    rows = convert_types(rows, [str, float, float])
+    rows = make_dicts(rows, ['name', 'price', 'change'])
+    return rows
+
+def ticker(portfile, logfile, fmt):
+    portfolio = read_portfolio(portfile)
+    lines = follow(logfile)
+    rows = parse_stock_data(lines)
+    rows = filter_symbols(rows, portfolio)
+    formatter = create_formatter(fmt)
+    formatter.headings(['Name', 'Price', 'Change'])
+    for row in rows:
+        rowdata = [row['name'], row['price'], row['change']]
+        formatter.row(rowdata)
+
+if __name__ == '__main__':
+    ticker('Data/portfolio.csv', 'Data/stocklog.csv', 'txt')
